345fe637c3a6e0f914cde66b68804cc97fb88850
sj-hx17 <sheena27gutierrez@gmail.com>
2025-05-28 11:58:27 +0800

    Update and rename index.html to Home.php

--- Home.php
+++ Home.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>HoloFo MaE</title>
+        <title>HoloFo MaEM</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
         <style>
             /* Basic styles for modal */
@@ -266,7 +266,7 @@
                         console.log(`Redirecting to ${type.charAt(0).toUpperCase() + type.slice(1)} login page.`);
                         if (type === 'admin') {
                             // Redirect to AdminLogin.html
-                            window.location.href = 'AdminLogin.html'; 
+                            window.location.href = 'AdminLogin.php'; 
                         } else if (type === 'frontdesk') {
                             // Redirect to FrontdeskLogin.html
                             window.location.href = 'FrontdeskLogin.php'; 
